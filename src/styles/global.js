7d8596ee6e10ebf3e6ffdfb61b06eb25703366ea
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-03-20 20:05:36 -0300

    feat: update app and add global style

---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Montserrat:700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
+`;
